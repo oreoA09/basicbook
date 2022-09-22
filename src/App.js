00c3ac63dd2ae42dd67262b5dee2328405042236
oreoA09 <oreo.akanni@gmail.com>
2022-09-22 23:51:42 +0000

    initial newbasicbook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {data} from './books'
+import SpecificBook from './Book'
+import {greeting} from './testing/testing'
 
 function App() {
+  console.log(greeting);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='booklist'>
+      {data.map((book, index) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
+      })}
+    </section>
   );
 }
 
-export default App;
+export default App
